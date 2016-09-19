1437015dbb5e09a08f05e6d070d28d3a26f14fdb
Francisco Calaça Xavier <chicocx@gmail.com>
2016-09-19 10:52:36 -0300

    First commit

---
+++ Assets/RestClient.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Experimental.Networking;
+
+public class RestClient : MonoBehaviour {
+
+    void Start()
+    {
+        StartCoroutine(GetText());
+    }
+
+    IEnumerator GetText()
+    {
+        UnityWebRequest www = UnityWebRequest.Get("http://localhost:8080/restserver-game/services/example/listItems");
+        yield return www.Send();
+
+        if (www.isError)
+        {
+            Debug.Log(www.error);
+        }
+        else
+        {
+            // Show json results as text
+            string json = www.downloadHandler.text;
+            Debug.Log(json);
+
+            // Or convert result from json
+            Item item = JsonUtility.FromJson<Item>(json);
+            Debug.Log("Name: " + item.name);
+            Debug.Log("Value: " + item.value);
+        }
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+"# Client Rest with Unity" 
